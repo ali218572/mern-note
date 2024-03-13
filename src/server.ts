e0ce4735f784149904606d9f27ad692b9c4602a2
ali mansour <ali218572@gmail.com>
2024-03-13 22:20:06 +0200

    Setting up & Using middlware 2

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import env from "./util/validateEnv";
+import app from "./app";
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port!, () => {
+      console.log("Server running on port " + port);
+    });
+  })
+  .catch(console.error);
